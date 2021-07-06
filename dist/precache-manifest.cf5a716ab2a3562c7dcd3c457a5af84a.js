aaabceacd71600813b71635daea29ec6a4e50a47
Pankaj <pankajjha0191@gmail.com>
2021-07-06 14:13:28 +0530

    new stage changes folder.

---
+++ dist/precache-manifest.cf5a716ab2a3562c7dcd3c457a5af84a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "504f48648f937ccabfa4",
+    "url": "/css/app.138240b5.css"
+  },
+  {
+    "revision": "280779f08c8d9888186ff02bd6dfbd0a",
+    "url": "/data-sources/clients.json"
+  },
+  {
+    "revision": "9ed33b873e49a44feb61a3e2fea0dc2a",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "2144ea8cf3f358d715ff4570fab0745f",
+    "url": "/img/justboil-logo.2144ea8c.svg"
+  },
+  {
+    "revision": "a902c42d31f8794d13cfc983358b4aee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "504f48648f937ccabfa4",
+    "url": "/js/app.236b12bc.js"
+  },
+  {
+    "revision": "71a26b94af9a2383a760",
+    "url": "/js/chunk-vendors.61cafa33.js"
+  },
+  {
+    "revision": "47f2fbbb4b906aecb385",
+    "url": "/js/client-form.32ff2d7a.js"
+  },
+  {
+    "revision": "5f69d9bd1d40fc618482",
+    "url": "/js/client-form~forms.af96e34e.js"
+  },
+  {
+    "revision": "185836bb34a899607f57",
+    "url": "/js/forms.f12b5c14.js"
+  },
+  {
+    "revision": "c1a00d542ae4b48dbb12",
+    "url": "/js/profile.006ced91.js"
+  },
+  {
+    "revision": "6c8ba56e4f1682ea474a",
+    "url": "/js/tables.fad85f26.js"
+  },
+  {
+    "revision": "9e02268cb14385af9653507bc8b8ee72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
